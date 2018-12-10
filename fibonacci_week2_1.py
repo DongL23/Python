c9f446bec44d85aa6dfc6bd4a179e0bfd9fdaeed
DongL23 <45372769+DongL23@users.noreply.github.com>
2018-12-10 15:23:56 -0500

    Delete fibonacci_week2_1.py

--- fibonacci_week2_1.py
+++
@@ -1,9 +0,0 @@
-# Uses python3
-def calc_fib(n):
-    if (n <= 1):
-        return n
-    for i in range(2, n):
-        return calc_fib(i - 1) + calc_fib(i - 2)
-
-n = int(input())
-print(calc_fib(n))
